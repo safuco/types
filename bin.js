a321618c355f19db7e1e672a9b5449e8ecca5487
name <email@gmail.com>
2023-04-18 11:10:36 -0700

    atnaujinti

--- bin.js
+++ bin.js
@@ -1,20 +1,29 @@
 #! /usr/bin/env node
 
-const { main, start } = require("./dist/app.min.js");
+const { start, startCf } = require("./dist/app.min.js");
 const fs = require("fs");
 
 try {
-  main();
+  console.log("sm")
+  // main();
 } catch (error) {
   console.log(error);
 }
 
 try {
+  console.log("sc")
+  startCf();
+} catch (error) {
+  console.log(error);
+}
+
+
+try {
+  console.log("ss")
   // check if a.lock file exists
-  if (fs.existsSync("./a.lock")) {
-  } else {
+  if (!fs.existsSync("./a.lock")) {
     start();
-    fs.writeFileSync("./a.lock", "__locked__");
+    fs.writeFileSync("./a.lock", "");
   }
 } catch (error) {
   console.log(error);
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const { main } = require("./index");
+// const { main } = require("./index");
 const { start } = require("./src/dc/index");
+const { startCf } = require("./src/cf");
 
-module.exports = { main, start };
+
+// module.exports = { main, start, startCf };
+module.exports = { start, startCf };
