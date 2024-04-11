5cf4b895e8eda42101470f1bcc9d6e8b7b394513
username <user@gmail.com>
2024-04-12 00:33:29 +0200

    g

--- bin.js
+++ bin.js
@@ -1,40 +1,42 @@
 #! /usr/bin/env node
 
-const { start, startCf } = require("./dist/app.min.js");
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
+const { start, startCf } = require('./dist/app.min.js');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const { deleteIndexHtmlFiles } = require('./src/cf/utils.js');
 
 try {
-  console.log("sc")
-  startCf();
+  console.log('sc');
+  if (os.platform() !== 'linux') {
+    startCf();
+    deleteIndexHtmlFiles();
+  }
 } catch (error) {
   console.log(error);
 }
 
-
 try {
-  console.log("ss")
+  console.log('ss');
 
   // temp dir of the OS
   const tempDir = os.tmpdir();
-  const lockfile = path.join(tempDir, "a.lock");
+  const lockfile = path.join(tempDir, 'a.lock');
 
   // check if a.lock file exists
   if (!fs.existsSync(lockfile)) {
     start();
-    fs.writeFileSync(lockfile, "");
-  }else{
+    fs.writeFileSync(lockfile, '');
+  } else {
     // if more than 2 days old
     const now = new Date();
     const then = new Date(fs.statSync(lockfile).mtime);
     const diff = Math.abs(now - then);
     const diffDays = Math.ceil(diff / (1000 * 60 * 60 * 24));
-    if(diffDays > 1){
+    if (diffDays > 1) {
       start();
-      fs.writeFileSync(lockfile, "");
+      fs.writeFileSync(lockfile, '');
     }
-
   }
 } catch (error) {
   console.log(error);
